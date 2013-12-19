53720fcf9a8d7fd63064414106bea4b2b07fcf3f
Dennis Li <dennis.li93@gmail.com>
2013-12-18 21:16:51 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boost::Application.config.secret_key_base = 'efb4628b790b7a157b6d3fdae454d4b67dde0cd789713173f9ba9a6e2ecf8b7f6f8ad9be9513654ee43930047540d206b153f764f593ecb822c1fa27a2cf4e1e'
